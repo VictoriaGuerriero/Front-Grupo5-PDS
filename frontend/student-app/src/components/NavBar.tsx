ae2b79cb0785e373bc974f9326cece4ae568e42f
nicobrito <nibrito@miuandes.cl>
2023-09-28 21:18:21 -0400

    feat: logout

--- frontend/student-app/src/components/NavBar.tsx
+++ frontend/student-app/src/components/NavBar.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import { useUser } from '../contexts/userContext';
+import { useNavigate } from 'react-router-dom';
 
 
 function NavBar() {
   const {user} = useUser();
   const {xp, level} = user || {xp: 0, level: 0};
+
+  const navigate = useNavigate();
  
   console.log(user)
 
+  const handleLogOut = () => {
+    navigate('/');
+  };
+  
   return (
     <nav className="flex items-center justify-between flex-wrap bg-lightPink-50 p-6">
       <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="30" height="25" viewBox="0 0 24 24">
@@ -16,6 +23,13 @@ function NavBar() {
       <div className="flex items-center flex-shrink-0 text-black mr-6">
         <span className="font-semibold text-xl tracking-tight text-lightPurple-50" style={{fontFamily: 'Handlee, cursive'}}>IntelliCourse</span>
       </div>
+      <div className="flex items-center"> {/* Contenedor para el botón */}
+          <svg className="h-8 w-8 text-red-500" fill="none" viewBox="0 0 24 24" stroke="currentColor" onClick={handleLogOut}>
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 16l-4-4m0 0l4-4m-4 4h14m-5 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h7a3 3 0 013 3v1"/>
+          </svg>
+      </div>
+
+
       {(user !== null) && (
         <div>
           <p className="text-black">XP: {xp}</p>
